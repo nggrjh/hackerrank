1f2d109d43465a6fb853f9ca7aba2fb8ef3adc7d
nggrjh <linggarismakingthings@gmail.com>
2023-05-14 23:25:38 +0800

    solved the-birthday-bar problem

---
+++ solution/the-birthday-bar.go
@@ -0,0 +1,36 @@
+package solution
+
+import "fmt"
+
+func birthday(s []int32, d, m int32) int32 {
+	ln := len(s)
+	mx := ln - int(m)
+
+	fmt.Println(ln, mx)
+
+	var tmp [][]int32
+	if mx < 1 {
+		tmp = append(tmp, s)
+	} else {
+		for i := 0; i <= mx; i++ {
+			var t []int32
+			for j := i; j < i+int(m); j++ {
+				t = append(t, s[j])
+			}
+			tmp = append(tmp, t)
+		}
+	}
+
+	count := int32(0)
+	for i := 0; i < len(tmp); i++ {
+		var sum int32
+		for j := 0; j < len(tmp[i]); j++ {
+			sum += tmp[i][j]
+		}
+		if sum == d {
+			count += 1
+		}
+	}
+
+	return count
+}
---
+++ solution/the-birthday-bar_test.go
@@ -0,0 +1,64 @@
+package solution
+
+import "testing"
+
+func Test_birthday(t *testing.T) {
+	type args struct {
+		s []int32
+		d int32
+		m int32
+	}
+	tests := map[string]struct {
+		args args
+		want int32
+	}{
+		"case_0.1": {
+			args: args{
+				s: []int32{2, 2, 1, 3, 2},
+				d: 4,
+				m: 2,
+			},
+			want: 2,
+		},
+		"case_0.2": {
+			args: args{
+				s: []int32{1, 2, 1, 3, 2},
+				d: 3,
+				m: 2,
+			},
+			want: 2,
+		},
+		"case_1": {
+			args: args{
+				s: []int32{1, 1, 1, 1, 1, 1},
+				d: 3,
+				m: 2,
+			},
+			want: 0,
+		},
+		"case_2": {
+			args: args{
+				s: []int32{4},
+				d: 4,
+				m: 1,
+			},
+			want: 1,
+		},
+		"case_10": {
+			args: args{
+				s: []int32{3, 5, 4, 1, 2, 5, 3, 4, 3, 2, 1, 1, 2, 4, 2, 3, 4, 5, 3, 1, 2, 5, 4, 5, 4, 1, 1, 5, 3, 1, 4, 5, 2, 3, 2, 5, 2, 5, 2, 2, 1, 5, 3, 2, 5, 1, 2, 4, 3, 1, 5, 1, 3, 3, 5},
+				d: 18,
+				m: 6,
+			},
+			want: 10,
+		},
+	}
+	for name, test := range tests {
+		tt := test
+		t.Run(name, func(t *testing.T) {
+			if got := birthday(tt.args.s, tt.args.d, tt.args.m); got != tt.want {
+				t.Errorf("birthday() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
